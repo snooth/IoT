788f98ba1d49adee9c7afd5f0fdc1b492e98dfae
snooth <snooth@gmail.com>
2019-11-13 21:35:17 +1100

    Create tempsensor.py

---
+++ tempsensor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import glob
+import time
+
+while True:
+   # change the device ID to whatever yours is. 
+   for sensor in glob.glob("/sys/bus/w1/devices/28-01145428d9aa/w1_slave"):
+      id = sensor.split("/")[5]
+
+      try:
+         f = open(sensor, "r")
+         data = f.read()
+         f.close()
+         if "YES" in data:
+            (discard, sep, reading) = data.partition(' t=')
+            t = float(reading) / 1000.0
+            print("{} {:.1f}".format(id, t))
+         else:
+            print("999.9")
+
+      except:
+         pass
+
+   time.sleep(3.0)
